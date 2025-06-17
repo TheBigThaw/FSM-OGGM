9499651dd0007464fe775a5b5c25a36ee8030616
bearecinos <beatriz.recinos@ed.ac.uk>
2025-06-17 16:24:51 +0100

    slurm scripts to run in forth

---
+++ slurm_scripts_rof/run_fsm_oggm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --account=geos_extra  # Ensure the job runs in the correct queue
+#SBATCH --nodelist=forth03
+#SBATCH --output=slurm_log.out
+#SBATCH --error=slurm_log.err
+#SBATCH --job-name=run_fsm_oggm
+#SBATCH --time=24:00:00  # Set max run time (adjust as needed)
+#SBATCH --mem=48GB
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16  # Adjust CPU allocation if needed
+#SBATCH --mail-type=ALL  # Notifications for job begin, end, and failure
+#SBATCH --mail-user=beatriz.recinos@ed.ac.uk  # Email for notifications
+
+# Load necessary modules (if required)
+source ~/.bashrc
+mamba activate oggm_fsm
+
+python test_fsm_rofental.py $(<params.txt)
