a784f8b432787b9f4ad9af441f4f79d960eb72b4
bearecinos <beatriz.recinos@ed.ac.uk>
2025-06-25 16:57:55 +0100

    first trial to test the scripts on forth

--- slurm_scripts_rof/test_fsm_rofental.py
+++ slurm_scripts_rof/test_fsm_rofental.py
@@ -1,6 +1,8 @@
 import argparse
 import os
 import geopandas as gpd
+import pandas as pd
+import numpy as np
 import xarray as xr
 import glob
 from oggm import cfg, utils
@@ -181,8 +183,53 @@ def main(args):
 
     final_d.to_netcdf(f_path)
 
-    print("DONE running distributed thickness")
+    print("DONE running distributed thickness postprocessing")
+    print("Now we start post-processing runoff")
 
+    dfinal = pd.DataFrame()
+
+    for gdir in gdirs:
+        with xr.open_dataset(gdir.get_filepath('FSM_runoff')) as dg:
+            dg_sim = dg.load()
+
+        calendar_year = dg_sim['time'].dt.year.values
+        calendar_month = dg_sim['time'].dt.month.values
+        calendar_day = dg_sim['time'].dt.day.values
+        time = dg_sim['time'].values
+        lat = np.repeat(np.nan, len(time))
+        lon = np.repeat(np.nan, len(time))
+        ice_melt_on_glacier = dg_sim.runoff_ice.values
+        snow_melt_on_glacier = dg_sim.snow_melt.values
+        id = np.repeat(rgi_id, len(time))
+
+        row = {'RGIID': id,
+               'calendar_year': calendar_year,
+               'calendar_month': calendar_month,
+               'calendar_day': calendar_day,
+               'time': time,
+               'lat': lat,
+               'lon': lon,
+               'ice_melt_on_glacier_daily': ice_melt_on_glacier,
+               'snow_melt_on_glacier_daily': snow_melt_on_glacier,
+               }
+
+        df = pd.DataFrame(row)
+        df = df.set_index(['RGIID', 'time'])
+        dfinal = pd.concat([dfinal, df])
+
+    ds_runoff = dfinal.to_xarray()
+
+    # Let's make a directory for CEH data and file formats
+    output_dir = os.path.join(cfg.PATHS['working_dir'],
+                              '02_run_off_terminus_position')
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    final_path_daily = os.path.join(output_dir,
+                                    'run_off_daily_and_terminus_position_' + 'climate_historical_fsm' + '.nc')
+
+    ds_runoff.to_netcdf(final_path_daily)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run FSM OGGM model with customizable parameters')
